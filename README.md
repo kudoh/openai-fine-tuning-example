54f4151f5446b7f07d89aa30e2bad63bc54642f6
noboru-kudo <noboru-kudo@mamezou.com>
2023-09-06 12:49:55 +0900

    feat: analyze fine-tuning result

--- README.md
+++ README.md
@@ -10,15 +10,31 @@ export OPENAI_API_KEY=<your api key>
 
 ## Train
 
-Specify a JSONL-formatted study file and execute the following.
+Specify a JSONL-formatted training file and execute the following.
 
 ```shell
-npm run train -- sample.jsonl
+# Optionally you can use n_epochs after file path
+npm run train sample.jsonl
 ```
 
 After a while, the fine-tuned model name will be output on the console.
 
-## Test
+## Analyze
+
+To check the result of fine-tuning, refer to job events or `result_file`.
+
+```shell
+# get result_file from training job
+npm run list-jobs
+
+# list job event
+npm run list-event <job-id>
+
+# show result-file
+npm run get-files <result_file-id>
+```
+
+## Test the fine-tuned model
 
 Test with a fine-tuned model name and any prompts.
 
--- train.ts
+++ train.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 const openapi = new OpenAI();
 
 async function train(args: string[]) {
-  const [file] = args;
+  const [file, epoch] = args;
   console.log('file', file);
   if (!file) throw new Error('no file given');
   const fileCreateResp = await openapi.files.create({
@@ -15,19 +15,22 @@ async function train(args: string[]) {
   while (true) {
     await new Promise(resolve => setTimeout(resolve, 10000));
     const files = await openapi.files.list();
-    const target = files.data.find(file => file.id === fileCreateResp.id)
+    const target = files.data.find(file => file.id === fileCreateResp.id);
     if (target) {
-      if (target.status === "processed") {
+      if (target.status === 'processed') {
         break;
       } else if (target.status === 'error' || target.status === 'deleting' || target.status === 'deleted') {
-        throw new Error(target.status_details || "no details");
+        throw new Error(target.status_details || 'no details');
       }
     }
   }
 
   const jobResp = await openapi.fineTuning.jobs.create({
     model: 'gpt-3.5-turbo',
-    training_file: fileCreateResp.id
+    training_file: fileCreateResp.id,
+    hyperparameters: {
+      n_epochs: epoch ? Number(epoch) : 'auto'
+    }
   });
   console.log(jobResp);
 
@@ -42,18 +45,35 @@ async function train(args: string[]) {
         modelName = target.fine_tuned_model;
         break;
       } else if (target.status === 'failed' || target.status === 'cancelled') {
-        throw new Error(`job failed. status:${target.status}`)
+        throw new Error(`job failed. status:${target.status}`);
       }
-      console.log("status:", target.status)
+      console.log('status:', target.status);
     } else {
-      throw new Error("job not found")
+      throw new Error('job not found');
     }
   }
   console.log('fine-tuned model name', modelName);
 }
 
 async function listJobs() {
-  console.log(await openapi.fineTuning.jobs.list());
+  const jobs = await openapi.fineTuning.jobs.list();
+  console.log(JSON.stringify(jobs.data.slice(0, 5), null, 2));
+  return jobs.data;
+}
+
+async function listJobEvent(args: string[]) {
+  const [jobId] = args;
+  const target = jobId ?? (await listJobs())[0].id;
+  const job = await openapi.fineTuning.jobs.listEvents(target);
+  console.log(JSON.stringify(job.data, null, 2));
+  return job;
+}
+
+async function getFile(args: string[]) {
+  const [fileId] = args;
+  if (!fileId) throw new Error('fileId not given');
+  const file = await openapi.files.retrieveContent(fileId);
+  console.log(file);
 }
 
 async function chat(args: string[]) {
@@ -75,7 +95,7 @@ if (!command) {
   console.log('no command');
   process.exit(1);
 }
-let fn: (...args: any[]) => Promise<void>;
+let fn: (...args: any[]) => Promise<unknown>;
 switch (command) {
   case 'train':
     fn = train;
@@ -85,6 +105,12 @@ switch (command) {
     break;
   case 'chat':
     fn = chat;
+    break;
+  case 'getFile':
+    fn = getFile;
+    break;
+  case 'listJobEvent':
+    fn = listJobEvent;
     break;
   default:
     console.log('illegal command', command);
